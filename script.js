6e407a8ce7f8ccea853c66c168817f1046d3e557
FenczikStudios <steven@fenczikstudios.com>
2023-09-08 12:26:50 -0600

    added guide and about pages
    they are empty

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Get references to the guide div and logo image
+const guideDiv = document.getElementById("guide");
+const logoImg = document.getElementById("logo");
+const aboutDiv = document.getElementById("about")
+
+// event listener for guide element
+guideDiv.addEventListener("click", function() {
+    window.location.href = "guide.html";
+    console.log = "clicked"
+});
+
+// event listener for about element
+aboutDiv.addEventListener("click", function() {
+    window.location.href = "about.html";
+});
+
+// event listener for logo
+logoImg.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
 
